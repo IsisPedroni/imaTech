cd4018161eb0046f047bdb15b2c863e9fc5a9fb3
Isis Pedroni <isispedroni3@gmail.com>
2023-08-02 20:40:48 -0300

    aula flutter

---
+++ flutter/aula01/lib/Texto.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+
+class Texto extends StatelessWidget {
+  // Atributo
+  String? nome;
+  Color? cor;
+  double? tamanhoFonte;
+
+  // Construtor
+  Texto({super.key});
+  Texto.obterNome({super.key, this.nome});
+  Texto.obterNomeCor({super.key, this.nome, this.cor});
+  Texto.obterNomeCorTamanhoFonte(
+      {super.key, this.nome, this.cor, this.tamanhoFonte});
+
+  // Método
+  @override
+  Widget build(BuildContext context) {
+    return Text('Hello $nome!',
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: tamanhoFonte, color: cor));
+  }
+}
