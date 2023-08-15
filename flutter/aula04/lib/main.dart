f06d8314be91f7332a368676fae1e55b1966fa05
Isis Pedroni <isispedroni3@gmail.com>
2023-08-14 22:00:34 -0300

    exercicio02

--- flutter/aula04/lib/main.dart
+++ flutter/aula04/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 247, 0, 136)),
       ),
-      home: const Pagina(),
+      home: Pagina(),
     );
   }
 }
