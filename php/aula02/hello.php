e48b7835af09c6d44f4f4eb12316121d60eed30d
Isis Pedroni <isispedroni3@gmail.com>
2023-09-09 14:05:31 -0300

    aula 01

---
+++ php/aula02/hello.php
@@ -0,0 +1,8 @@
+<!-- Sempre que codar em php tempo que colocar ele na TAG PHP -->
+<!-- Não é obrigatoria fechar a tag, só é fechada quando queremos usar o html junto. Para que conseguimos ver a separação -->
+
+<?php 
+// comando echo exibe no console
+echo 'Hello' . " World\n" ;
+
+print 'hello World';
---
+++ php/aula02/variaveis.php
@@ -0,0 +1,20 @@
+<?php
+// Liguem fracamente tipada
+
+// o sifrão $ define uma variavel
+// 
+$variavel = 'Texto';
+
+$number = 289.3;
+
+$number1 = 29;
+$number2 = 289;
+
+$nevativa = -288;
+
+$booleano = false;
+$booleano1 = true;
+
+$calculo = $number1 + $number2;
+
+echo $calculo;
